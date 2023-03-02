a59aecf16ad2a48e1d9b54eb575e5517a07e72cc
Jellinek <jellinekbraus@gmail.com>
2023-03-03 01:03:22 +0500

    start

---
+++ myproject/oceanview-frontend/vue.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require('@vue/cli-service')
+const BundleTracker = require('webpack-bundle-tracker');
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: "http://0.0.0.0:8080",
+  outputDir: "./dist/",  
+
+  chainWebpack: config => {
+      config.optimization.splitChunks(false)
+
+      config.plugin('BundleTracker').use(BundleTracker, [
+          {
+              filename: './webpack-stats.json'
+          }
+      ])
+
+      config.resolve.alias.set('__STATIC__', 'static')
+
+      config.devServer
+          .host('0.0.0.0')
+          .port(8080)
+          .hot(true)
+          .https(false)
+          .headers({'Access-Control-Allow-Origin': ['\*']})
+      config.watchOptions({
+        poll: 1000
+      })
+  }
+})
