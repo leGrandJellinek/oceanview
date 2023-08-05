1a6b1b42794668a915a4ff5f142126324875454c
iibratik <iibragimov2417@gamil.com>
2023-08-05 21:45:32 +0500

    all changes

--- myproject/oceanview-frontend/vue.config.js
+++ myproject/oceanview-frontend/vue.config.js
@@ -1,40 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-const BundleTracker = require('webpack-bundle-tracker');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/dist/'
-  : '/',
-  outputDir: "./dist/",
-
-  chainWebpack: config => {
-      config.optimization.splitChunks(false)
-
-      config.plugin('BundleTracker').use(BundleTracker, [
-          {
-              filename: './webpack-stats.json'
-          }
-      ])
-
-      config.resolve.alias.set('__STATIC__', 'static')
-
-      config.devServer
-          .host('localhost')
-          .port(8080)
-          .hot(true)
-          .https(false)
-          .historyApiFallback(true)
-          .headers({'Access-Control-Allow-Origin': ['\*']})
-      config.watchOptions({
-        poll: 1000
-      })
-  },
-  chainWebpack: (config) => {
-    config.module
-      .rule('pug')
-      .test(/\.pug$/)
-      .use('pug-plain-loader')
-      .loader('pug-plain-loader')
-      .end();
-  },
+  transpileDependencies: true
 })
